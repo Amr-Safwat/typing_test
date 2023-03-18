5ea358b8b05cbe690d2a14ac4aee8a24ce2901cf
Amr Safwat <amrsafwat02@gmail.com>
2023-03-18 20:33:06 +0200

    Added some Modifiers

--- main.js
+++ main.js
@@ -1,46 +1,46 @@
 // Array Of Words
 const words = [
-  "Hello",
-  "Programming",
-  "Code",
-  "Javascript",
-  "Town",
-  "Country",
-  "Testing",
-  "Youtube",
-  "Linkedin",
-  "Twitter",
-  "Github",
-  "Leetcode",
-  "Internet",
-  "Python",
-  "Scala",
-  "Destructuring",
-  "Paradigm",
-  "Styling",
-  "Cascade",
-  "Documentation",
-  "Coding",
-  "Funny",
-  "Working",
-  "Dependencies",
-  "Task",
-  "Runner",
-  "Roles",
-  "Test",
-  "Rust",
-  "Playing"
+	'Hello',
+	'Programming',
+	'Code',
+	'Javascript',
+	'Town',
+	'Country',
+	'Testing',
+	'Youtube',
+	'Linkedin',
+	'Twitter',
+	'Github',
+	'Leetcode',
+	'Internet',
+	'Python',
+	'Scala',
+	'Destructuring',
+	'Paradigm',
+	'Styling',
+	'Cascade',
+	'Documentation',
+	'Coding',
+	'Funny',
+	'Working',
+	'Dependencies',
+	'Task',
+	'Runner',
+	'Roles',
+	'Test',
+	'Rust',
+	'Playing',
 ];
 
 // Setting Levels
 const Levels = {
-  "Easy": 5,
-  "Normal": 3,
-  "Hard": 2,
-}
+	Easy: 5,
+	Normal: 3,
+	Hard: 2,
+};
 
 // Default Level
-let defaultLevelName = 'Normal';
+let defaultLevelName = 'Easy';
 let defaultLevelSeconds = Levels[defaultLevelName];
 
 // catch Selectors
@@ -63,21 +63,68 @@ scoreTotal.innerHTML = words.length;
 
 // Disable Paste Event
 input.onpaste = function () {
-  return false;
-}
+	return false;
+};
 
 // Start Game
 startButton.onclick = () => {
-  startButton.remove();
-  input.focus()
-  // Generate Word Function
-  generateWords();
-}
+	startButton.remove();
+	input.focus();
+	// Generate Word Function
+	generateWords();
+};
 
 function generateWords() {
-  let randomWord = words[Math.floor(Math.random()*words.length)];
-  theWord.innerHTML = randomWord;
-  console.log(randomWord);
+	let randomWord = words[Math.floor(Math.random() * words.length)];
+	// Get Word Index
+	let index = words.indexOf(randomWord);
+	// Remove Word From Array
+	words.splice(index, 1);
+	// Empty Upcoming Words
+	upComingWords.innerHTML = '';
+	// Generate Words
+	for (let i = 0; i < words.length; i++) {
+		let div = document.createElement('div');
+		let word = document.createTextNode(words[i]);
+		div.appendChild(word);
+		upComingWords.appendChild(div);
+	}
+	theWord.innerHTML = randomWord;
+	// call start play function
+	startPlay();
 }
-// console.log(Levels['Normal']);
 
+function startPlay() {
+	timeLeft.innerHTML = defaultLevelSeconds;
+	let id = setInterval(() => {
+		timeLeft.innerHTML--;
+		if (timeLeft.innerHTML === '0') {
+			clearInterval(id);
+			if (
+				input.value.toLocaleLowerCase() ===
+				theWord.innerHTML.toLocaleLowerCase()
+			) {
+				input.value = '';
+				scoreGot.innerHTML++;
+				// check if array is not empty
+				if (words.length > 0) {
+					generateWords();
+				} else {
+					let span = document.createElement('span');
+					let txtSpan = document.createTextNode('congratulations🥳');
+					span.className = 'good';
+					span.appendChild(txtSpan);
+					finish.appendChild(span);
+				}
+			} else {
+				let span = document.createElement('span');
+				let txtSpan = document.createTextNode('Game Over');
+				span.className = 'bad';
+				span.appendChild(txtSpan);
+				finish.appendChild(span);
+			}
+		}
+	}, 1000);
+}
+
+// console.log(Levels['Normal']);
